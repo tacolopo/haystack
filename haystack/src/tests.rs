cc7d7201e9d3d82668ac2487a79582f7827977bb
tacolopo <anthonyrosa@alxandria.org>
2023-01-08 15:14:34 -0500

    Revert "Revert "commit to revert""
    This reverts commit 94f485bac2afdf50aeb92f6027149fff8eb7e94e.

--- haystack/src/tests.rs
+++ haystack/src/tests.rs
@@ -1,7 +1,7 @@
-use crate::contract::{execute, instantiate, query};
-use crate::msg::{AllRecipientsResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::contract::{execute, instantiate};
+use crate::msg::{ExecuteMsg, InstantiateMsg};
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-use cosmwasm_std::{attr, coin, from_binary};
+use cosmwasm_std::{attr, coin};
 
 pub const ADDR1: &str = "secret1993j5gsv2m3eqlkh7a9hvv8qdrwyr0k7pq5tua";
 
@@ -107,26 +107,10 @@ fn test_execute_deposit_10() {
         output_address: "secret1f80fazgdukg2wg6wcxhcvau396kdrxjvltp20d".to_string(),
     };
     let _res = execute(deps.as_mut(), env.clone(), info, msg).unwrap();
-    //query all recipients to assert they are at 9
-    let msg = QueryMsg::AllRecipients {
-        limit: None,
-        start_after: None,
-    };
-    let bin = query(deps.as_ref(), env.clone(), msg).unwrap();
-    let res: AllRecipientsResponse = from_binary(&bin).unwrap();
-    assert_eq!(res.recipients.len(), 9);
     //execute ten deposit
     let info = mock_info(ADDR1, &[coin(10_000_000, SCRT)]);
     let msg = ExecuteMsg::Deposit {
         output_address: "secret1f80fazgdukg2wg6wcxhcvau396kdrxjvltp20d".to_string(),
     };
-    let _res = execute(deps.as_mut(), env.clone(), info, msg).unwrap();
-    //query all recipients to assert they have reset to 0
-    let msg = QueryMsg::AllRecipients {
-        limit: None,
-        start_after: None,
-    };
-    let bin = query(deps.as_ref(), env, msg).unwrap();
-    let res: AllRecipientsResponse = from_binary(&bin).unwrap();
-    assert_eq!(res.recipients.len(), 0);
+    let _res = execute(deps.as_mut(), env, info, msg).unwrap();
 }
